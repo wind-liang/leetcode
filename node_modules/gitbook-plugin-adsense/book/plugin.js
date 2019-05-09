d9ee50bd5e196079427e79bf63e1113dc145bff6
windliang <648917857@qq.com>
2019-05-10 00:33:59 +0800

    ad

--- node_modules/gitbook-plugin-adsense/book/plugin.js
+++ node_modules/gitbook-plugin-adsense/book/plugin.js
@@ -4,57 +4,23 @@ require(['gitbook'], function (gitbook) {
   var adInsertPoint = '.page-inner section'
     , adPosition = 'bottom'
     , ad;
-
+  function insert(){
+      var h = document.getElementsByTagName('head')[0];
+      var s1 = document.createElement('script');
+      s1.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+      s1.setAttribute('async', true);
+      h.appendChild(s1);
+      var s2 = document.createElement('script'); 
+      s2.innerHTML = '(adsbygoogle = window.adsbygoogle || []).push({google_ad_client: "ca-pub-123456789",enable_page_level_ads: true});'
+      h.appendChild(s2);
+      
+  }
   gitbook.events.bind('start', function (e, cnf) {
-    const config = cnf.adsense;
-
-    // Use custom insert point if defined
-    if (config.element) {
-      adInsertPoint = config.element;
-    }
-
-    // Use custom position if defined
-    if (config.position) {
-      adPosition = config.position;
-    }
-
-    // Inject script to head.
-    var adsByGoogleScript = document.createElement('script');
-    adsByGoogleScript.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
-    adsByGoogleScript.setAttribute('async', true);
-    document.body.appendChild(adsByGoogleScript);
-
-    // Generate <ins> (ad)
-    ad = document.createElement('ins');
-    ad.style = 'display: block';
-    ad.className = 'adsbygoogle';
-
-    ad.setAttribute('data-ad-client', config.client);
-    ad.setAttribute('data-ad-slot', config.slot);
-    ad.setAttribute('data-ad-format', config.format);
-
-    // Add the ad to the DOM
-    var element = document.querySelector(adInsertPoint);
-
-    if (adPosition === 'top') {
-      element.insertBefore(ad, element.firstChild);
-    } else {
-      element.appendChild(ad);
-    }
-
-    (adsbygoogle = window.adsbygoogle || []).push({}); // eslint-disable-line
+     insert();
   });
 
   // I insert ad again when switching pages
   gitbook.events.bind('page.change', function () {
-    if (ad) {
-      var element = document.querySelector(adInsertPoint);
-
-      if (adPosition === 'top') {
-        element.insertBefore(ad, element.firstChild);
-      } else {
-        element.appendChild(ad);
-      }
-    }
+    insert();
   });
 });
