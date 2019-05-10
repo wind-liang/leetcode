4eeebfbd40f60acb3e84e5728e97b1a2ab1990ef
windliang <648917857@qq.com>
2019-05-10 13:54:16 +0800

    verify

--- gitbook.sh
+++ gitbook.sh
@@ -1,10 +1,1 @@
-gitbook build;
-git add .;
-git commit -m $1;
-git push;
-git checkout gh-pages;
-cp -r _book/* .;
-git add .;
-git commit -m $1;
-git push;
-git checkout master;
+gitbook build && git add .&& git commit -m $1&& git push && git checkout gh-pages && cp -r _book/* .&& git add .&& git commit -m $1 && git push && git checkout master
