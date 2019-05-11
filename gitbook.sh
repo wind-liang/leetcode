8a7cb4bdb8771eb4dded7344643a75ab19789c63
windliang <648917857@qq.com>
2019-05-11 16:24:21 +0800

    70

--- gitbook.sh
+++ gitbook.sh
@@ -1,1 +1,1 @@
-gitbook build && git add .&& git commit -m $1&& git push && git checkout gh-pages && cp -r _book/* . && git add .&& git commit -m $1 && git push && git checkout master
+git add .&& git commit -m $1&& git push
